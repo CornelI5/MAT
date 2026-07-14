bb3cfb7b2fe5a5c20a8f49fd94abef03fde54a62
CornelI5 <kenzoyooo0@gmail.com>
2026-07-14 13:05:54 +0700

    Update analyzer.h

--- src/analyzer.h
+++ src/analyzer.h
@@ -1,11 +1,21 @@
 #ifndef ANALYZER_H
 #define ANALYZER_H
 
-#include <windows.h>
+#include <stdint.h>
+
+typedef struct {
+    uint16_t e_magic;
+    uint32_t e_lfanew;
+} DOS_HEADER;
+
+typedef struct {
+    uint32_t Signature;
+    uint16_t Machine;
+    uint16_t NumberOfSections;
+    uint32_t TimeDateStamp;
+} FILE_HEADER;
 
 void print_header(const char* title);
-void print_compile_time(DWORD timestamp);
-void calculate_md5(char* filename);
-void analyze_pe(char* filename);
+void analyze_pe(const char* filename);
 
 #endif
