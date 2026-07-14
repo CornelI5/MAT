3fb75ff26c1de7b4f4439b259a9393330d81d60d
CornelI5 <kenzoyooo0@gmail.com>
2026-07-14 13:11:20 +0700

    Update analyzer.h

--- src/analyzer.h
+++ src/analyzer.h
@@ -15,7 +15,17 @@ typedef struct {
     uint32_t TimeDateStamp;
 } FILE_HEADER;
 
+typedef struct {
+    char Name[8];
+    uint32_t VirtualSize;
+    uint32_t VirtualAddress;
+    uint32_t SizeOfRawData;
+    uint32_t Characteristics;
+} SECTION_HEADER;
+
 void print_header(const char* title);
+void print_compile_time(uint32_t timestamp);
+void calculate_md5(const char* filename);
 void analyze_pe(const char* filename);
 
 #endif
