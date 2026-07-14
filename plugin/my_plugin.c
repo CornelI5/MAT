83740953f254ef5a537ea7db1b3e7c7d62c589a1
CornelI5 <kenzoyooo0@gmail.com>
2026-07-14 20:58:55 +0700

    Create my_plugin.c

---
+++ plugin/my_plugin.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void run_plugin(const char* target_file) {
+    printf("[Plugin Execution Log] Analyzing security markers for %s...\n", target_file);
+    printf("[Plugin Execution Log] All checks completed dynamically!\n");
+}
