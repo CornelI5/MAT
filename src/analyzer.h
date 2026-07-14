486f0667d5d1b576913f9160d0615b291dc7fd59
CornelI5 <kenzoyooo0@gmail.com>
2026-07-14 13:23:52 +0700

    Update analyzer.h

--- src/analyzer.h
+++ src/analyzer.h
@@ -13,6 +13,10 @@ typedef struct {
     uint16_t Machine;
     uint16_t NumberOfSections;
     uint32_t TimeDateStamp;
+    uint32_t PointerToSymbolTable;
+    uint32_t NumberOfSymbols;
+    uint16_t SizeOfOptionalHeader;
+    uint16_t Characteristics;
 } FILE_HEADER;
 
 typedef struct {
@@ -20,6 +24,11 @@ typedef struct {
     uint32_t VirtualSize;
     uint32_t VirtualAddress;
     uint32_t SizeOfRawData;
+    uint32_t PointerToRawData;
+    uint32_t PointerToRelocations;
+    uint32_t PointerToLineNumbers;
+    uint16_t NumberOfRelocations;
+    uint16_t NumberOfLineNumbers;
     uint32_t Characteristics;
 } SECTION_HEADER;
 
@@ -27,5 +36,7 @@ void print_header(const char* title);
 void print_compile_time(uint32_t timestamp);
 void calculate_md5(const char* filename);
 void analyze_pe(const char* filename);
+uint32_t rva_to_offset(uint32_t rva, SECTION_HEADER* sections, int num_sections);
+void print_import_table(void* lpBase, uint32_t rva, uint32_t size, SECTION_HEADER* sections, int num_sections);
 
 #endif
